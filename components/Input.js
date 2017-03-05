fa481e0fe1383dbee859d91461d4996e6a0eea19
Feross Aboukhadijeh <feross@feross.org>
2017-03-04 21:09:44 -0800

    input component: bug fixes

--- components/Input.js
+++ components/Input.js
@@ -1,7 +1,6 @@
 const { Component, h } = require('preact') /** @jsx h */
 
 const c = require('classnames')
-const randombytes = require('randombytes')
 
 class Input extends Component {
   static get defaultProps () {
@@ -13,32 +12,38 @@ class Input extends Component {
 
   constructor () {
     super()
-    this.state.id = randombytes(1).toString('hex') // enforce unique html ids
+    this.id = 'input-' + Math.random().toString(16).slice(2, 8)
   }
 
-  render (props, state) {
-    if (!props.label) throw new Error('"label" prop is required')
-
-    const id = props.label.toLowerCase() + '-' + state.id
-
+  render (props) {
     const $optional = !props.required
       ? <span class='normal black-60'>(optional)</span>
       : null
 
+    const $label = props.label
+      ? <label for={this.id} class='f5 b db mb2'>{props.label} {$optional}</label>
+      : null
+
+    const descId = props.desc
+      ? 'desc-' + this.id
+      : null
+
     const $desc = props.desc
-      ? <small id={id + '-desc'} class='f6 black-60 db mb2'>{props.desc}</small>
+      ? <small id={descId} class='f6 black-60 db mb2'>{props.desc}</small>
       : null
 
     return (
       <div class={c('mb4', props.class)}>
-        <label for={id} class='f5 b db mb2'>{props.label} {$optional}</label>
+        {$label}
         <input
-          id={id}
+          id={this.id}
           type={props.type}
           class='input-reset ba b--black-20 pa2 mb2 db w-100'
           placeholder={props.placeholder}
           required={props.required}
-          aria-describedby={id + '-desc'}
+          aria-describedby={descId}
+          value={props.value}
+          onChange={props.onChange}
         />
         {$desc}
       </div>
