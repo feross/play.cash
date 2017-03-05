d305d9d5d5630287b7fc5fbd0df2d2931ea52ba2
Feross Aboukhadijeh <feross@feross.org>
2017-03-04 17:32:27 -0800

    form: convert to stateless component

--- components/Form.js
+++ components/Form.js
@@ -1,26 +1,18 @@
-const { Component, h } = require('preact') /** @jsx h */
+const { h } = require('preact') /** @jsx h */
 
 const c = require('classnames')
 
-class Form extends Component {
-  static get defaultProps () {
-    return {
-      label: null
-    }
-  }
+const Form = (props) => {
+  const $label = props.label
+    ? <legend class='f3 fw6 ph0 mh0'>{props.label}</legend>
+    : null
 
-  render (props) {
-    const $label = props.label
-      ? <legend class='f3 fw6 ph0 mh0'>{props.label}</legend>
-      : null
-
-    return (
-      <form class={c('pv2 black-80 tl measure', props.class)}>
-        {$label}
-        {props.children}
-      </form>
-    )
-  }
+  return (
+    <form class={c('pv2 black-80 tl measure', props.class)}>
+      {$label}
+      {props.children}
+    </form>
+  )
 }
 
 module.exports = Form
