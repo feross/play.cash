736be561e4d65a52cec1ae151d1a2c807789e5fc
Feross Aboukhadijeh <feross@feross.org>
2017-03-04 17:32:52 -0800

    sheet: convert to stateless component

--- components/Sheet.js
+++ components/Sheet.js
@@ -1,15 +1,11 @@
-const { Component, h } = require('preact') /** @jsx h */
+const { h } = require('preact') /** @jsx h */
 
 const c = require('classnames')
 
-class Sheet extends Component {
-  render (props) {
-    return (
-      <div class={c('mh2 ph4 pv4 br3 bg-white', props.class)}>
-        {props.children}
-      </div>
-    )
-  }
-}
+const Sheet = (props) => (
+  <div class={c('mh2 ph4 pv4 br3 bg-white-80', props.class)}>
+    {props.children}
+  </div>
+)
 
 module.exports = Sheet
