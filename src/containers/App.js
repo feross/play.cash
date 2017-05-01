17b58d86453e7ceb28642e59683c8579e941b780
Feross Aboukhadijeh <feross@feross.org>
2017-05-01 12:48:54 -0700

    app: put page requires on same line as friendly-name

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,27 +6,22 @@ const Header = require('./Header')
 const Player = require('./Player')
 // const Footer = require('../components/Footer')
 
-const HomePage = require('./HomePage')
-const TrackPage = require('./TrackPage')
-const NotFoundPage = require('./NotFoundPage')
-
 const PAGES = {
-  'home': HomePage,
-  'track': TrackPage,
-  'not-found': NotFoundPage
+  'home': require('./HomePage'),
+  'track': require('./TrackPage'),
+  'not-found': require('./NotFoundPage')
 }
 
 const App = (props) => {
   const { location } = store
 
-  const Page = PAGES[location.name]
-  const $page = Page ? <Page /> : <NotFoundPage />
+  const Page = PAGES[location.name] || PAGES['not-found']
 
   return (
     <div id='app' class='near-black f5'>
       <Player />
       <Header />
-      {$page}
+      <Page />
     </div>
   )
 }
