9306b35b19da89b4d6c662aaf34c72e7acd98994
Feross Aboukhadijeh <feross@feross.org>
2017-05-02 00:38:21 -0700

    location: extract router for standalone use (server-side, util fns)

--- src/lib/location.js
+++ src/lib/location.js
@@ -1,19 +1,15 @@
 // TODO: publish to npm
 
 const History = require('./history')
-const pathToRegexp = require('path-to-regexp')
+const Router = require('./router')
 
 class Location {
   constructor (routes, onChange) {
-    this._routes = routes.map(route => {
-      const [name, path] = route
-      const keys = []
-      const regexp = pathToRegexp(path, keys)
-      return { name, path, keys, regexp }
-    })
     this._onChange = onChange
     this._onHistoryChange = this._onHistoryChange.bind(this)
     this._onClick = this._onClick.bind(this)
+
+    this._router = new Router(routes)
 
     this._history = new History()
     this._history.on('change', this._onHistoryChange)
@@ -42,14 +38,14 @@ class Location {
 
     document.removeEventListener('click', this._onClick)
 
-    this._routes = null
     this._onChange = null
     this._onHistoryChange = null
+    this._router = null
     this._history = null
   }
 
   _onHistoryChange (pathname) {
-    const loc = this._parse(pathname)
+    const loc = this._router.match(pathname)
     this._onChange(loc)
   }
 
@@ -88,30 +84,6 @@ class Location {
 
     const pathname = el.pathname + el.search + (el.hash || '')
     this.push(pathname)
-  }
-
-  _parse (pathname) {
-    const ret = {
-      name: null,
-      params: {},
-      pathname
-    }
-    for (const route of this._routes) {
-      const matches = route.regexp.exec(pathname)
-      if (!matches) continue
-
-      // Found a matching route
-      ret.name = route.name
-      matches.slice(1).forEach((paramValue, paramIndex) => {
-        const param = route.keys[paramIndex].name
-        // Remove URL encoding from the param values. Accommodates whitespace in both
-        // x-www-form-urlencoded and regular percent-encoded form.
-        paramValue = decodeURIComponent(paramValue.replace(/\+/g, ' '))
-        ret.params[param] = paramValue
-      })
-      break
-    }
-    return ret
   }
 }
 
