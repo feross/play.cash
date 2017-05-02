a897454f89d2b41ad1a1c8bbc2f7bd4c22dbd101
Feross Aboukhadijeh <feross@feross.org>
2017-05-01 20:34:21 -0700

    add Image component

---
+++ src/components/Image.js
@@ -0,0 +1,22 @@
+const { h } = require('preact') /** @jsx h */
+
+const Image = (props) => {
+  const { src, sizes, sizeHint, alt } = props
+
+  const sources = typeof src === 'string' ? [src] : src
+  const srcset = sizes
+    ? sources.map((source, i) => `${source} ${sizes[i]}w`).join(', ')
+    : null
+
+  return (
+    <img
+      src={sources[0]}
+      srcset={srcset}
+      sizes={sizeHint}
+      alt={alt}
+      class={props.class}
+    />
+  )
+}
+
+module.exports = Image
