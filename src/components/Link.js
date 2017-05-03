0a03c8e572e5067c887b9c336ace569e35f3b7ca
Feross Aboukhadijeh <feross@feross.org>
2017-05-02 22:08:50 -0700

    Link: option to disable default color

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,12 +4,15 @@ const c = require('classnames')
 const Link = (props) => {
   const {
     href = '#',
+    defaultColor = true,
     onClick = () => {}
   } = props
 
+  const colorCls = defaultColor && 'blue hover-light-blue'
+
   return (
     <a
-      class={c('blue hover-light-blue link', props.class)}
+      class={c('link', colorCls, props.class)}
       href={href}
       onClick={e => {
         if (href === '#') e.preventDefault()
