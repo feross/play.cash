5cd30872158140de697ce7dbcf566bd906676a97
Feross Aboukhadijeh <feross@feross.org>
2017-05-10 15:40:59 -0700

    NotFoundPage: simplify it

--- src/containers/NotFoundPage.js
+++ src/containers/NotFoundPage.js
@@ -1,26 +1,13 @@
-const { Component, h } = require('preact') /** @jsx h */
-
-const store = require('../store')
+const { h } = require('preact') /** @jsx h */
 
 const ContentSheet = require('../components/ContentSheet')
 
-class NotFoundPage extends Component {
-  componentDidMount () {
-    const { location } = store
-    const q = location.pathname.replace(/\/|-/g, ' ').trim()
-
-    store.dispatch('FETCH_SEARCH', { q })
-  }
-
-  render (props) {
-    return (
-      <ContentSheet>
-        <h1>Error – Page Not Found</h1>
-        <h2>Maybe you were looking for one of these?</h2>
-        <p>TODO</p>
-      </ContentSheet>
-    )
-  }
+const NotFoundPage = (props) => {
+  return (
+    <ContentSheet>
+      <h1>Error – Page Not Found</h1>
+    </ContentSheet>
+  )
 }
 
 module.exports = NotFoundPage
