546a4662a02cff30cfc6b725427a787962b3e1c0
Feross Aboukhadijeh <feross@feross.org>
2019-10-02 14:39:04 -0700

    remove downgrade and unlimited

--- server/index.js
+++ server/index.js
@@ -1,18 +1,14 @@
 require('./rollbar')
 
 const ConnectSQLite = require('connect-sqlite3')
-const downgrade = require('downgrade')
 const http = require('http')
 const path = require('path')
 const session = require('express-session')
-const unlimited = require('unlimited')
 
 const app = require('./app')
 const config = require('../config')
 
 const PORT = process.argv[2] || 4000
-
-unlimited() // Upgrade the max file descriptor limit
 
 const server = http.createServer()
 server.listen(PORT, onListening)
@@ -20,8 +16,6 @@ server.listen(PORT, onListening)
 function onListening (err) {
   if (err) throw err
   console.log('Listening on port %s', server.address().port)
-
-  downgrade() // Set the process user identity to 'www-data'
 
   // Open DB as 'www-data' user
   const SQLiteStore = ConnectSQLite(session)
