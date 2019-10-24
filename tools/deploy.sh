d7d5984cbe40bfa7d0a1f9f4aaf780a1919af121
Feross Aboukhadijeh <feross@feross.org>
2019-10-23 18:58:25 -0700

    Update deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,29 +1,33 @@
 #!/bin/sh
-# Update code and restart server (run from app server)
+# Update code and restart server (run on server)
 set -e
 
-if [ -d "/home/feross/www/play.cash-build" ]; then
+if [ -d "/home/feross/www/build-play.cash" ]; then
   echo "ERROR: Build folder already exists. Is another build in progress?"
   exit 1
 fi
 
-cp -R /home/feross/www/play.cash /home/feross/www/play.cash-build
+if [ -d "/home/feross/www/old-play.cash" ]; then
+  echo "ERROR: Old folder exists. Did a previous build crash?"
+  exit 1
+fi
 
-cd /home/feross/www/play.cash-build && git pull
-cd /home/feross/www/play.cash-build && rm -rf node_modules
-cd /home/feross/www/play.cash-build && npm install --no-progress
-cd /home/feross/www/play.cash-build && npm run build
-cd /home/feross/www/play.cash-build && npm prune --production
+cp -R /home/feross/www/play.cash /home/feross/www/build-play.cash
+
+cd /home/feross/www/build-play.cash && git pull
+cd /home/feross/www/build-play.cash && npm install --no-progress
+cd /home/feross/www/build-play.cash && npm run build
+cd /home/feross/www/build-play.cash && npm prune --production --no-progress
 
 sudo supervisorctl stop play
 
 # Move database files (while app is stopped)
-cd /home/feross/www && rm -rf play.cash-build/db
-cd /home/feross/www && mv play.cash/db play.cash-build/db
+cd /home/feross/www && rm -rf build-play.cash/db
+cd /home/feross/www && mv play.cash/db build-play.cash/db
 
-cd /home/feross/www && mv play.cash play.cash-old
-cd /home/feross/www && mv play.cash-build play.cash
+cd /home/feross/www && mv play.cash old-play.cash
+cd /home/feross/www && mv build-play.cash play.cash
 
 sudo supervisorctl start play
 
-cd /home/feross/www && rm -rf play.cash-old
+cd /home/feross/www && rm -rf old-play.cash
